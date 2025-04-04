1457d93eebeb2cb76970e9c7baeaad690a76dc15
Vadim <Kozlov67b@gmail.com>
2025-04-04 18:46:50 +0300

    hw_37: добавлены маршруты и представления

--- barbershop/core/views.py
+++ barbershop/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def landing(request):
+    return render(request, 'landing.html')
+
+def thanks(request):
+    return render(request, 'core/thanks.html')
+
+def orders_list(request):
+    orders = [
+        # Ваши данные заявок
+    ]
+    return render(request, 'core/orders_list.html', {'orders': orders})
+
+def order_detail(request, order_id):
+    order = next((o for o in orders if o['id'] == order_id), None)
+    return render(request, 'core/order_detail.html', {'order': order})
